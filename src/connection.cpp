329e0b885e5f4bf5b010e1492ace800e2d7e2630
fdellekart <florian.dellekart@hotmail.de>
2024-04-19 21:15:39 +0200

    feat(connection): Do not read fixed length from socket but until it is finished

--- src/connection.cpp
+++ src/connection.cpp
@@ -12,7 +12,16 @@
 #include "response.h"
 
 // Number of bytes read from socket per request
-#define MAX_REQUEST_SIZE 1024
+#define MAX_REQUEST_SIZE 1024 * 100 // 100 kB
+
+/// @brief Check if the last four bytes read from request are two CRLF
+/// @param message pointer to the receiving message array
+/// @param buf_idx current number of bytes read
+/// @return boolean indicating if the request is finished
+bool terminate_read(char *message, int buf_idx) {
+  return message[buf_idx - 3] == '\r' & message[buf_idx - 2] == '\n' &
+         message[buf_idx - 1] == '\r' & message[buf_idx] == '\n';
+}
 
 void Connection::reply(Response &response) {
   std::string str_response = response.str();
@@ -21,9 +30,16 @@ void Connection::reply(Response &response) {
 }
 
 Request Connection::read_request() {
-  std::string recv_message(MAX_REQUEST_SIZE, 0);
-  read(socket, &recv_message[0], MAX_REQUEST_SIZE);
+  char recv_message[MAX_REQUEST_SIZE] = {0};
+  int buf_idx = -1;
+
+  do {
+    buf_idx++;
+    read(socket, &recv_message[buf_idx], 1);
+  } while (!terminate_read(recv_message, buf_idx));
+
+  std::string rcv_str = recv_message;
   Request request;
-  request.parse_string(recv_message);
+  request.parse_string(rcv_str);
   return request;
 }
